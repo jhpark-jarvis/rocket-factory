fe110283e294981308f5a88fc02891bb3b11a4e0
WooPJ <sindol256@gmail.com>
2026-05-11 01:07:41 +0900

    🗃️ USP_User_Insert.sql 프로시저 추가가

---
+++ sql/USP/USP_User_Insert.sql
@@ -0,0 +1,67 @@
+DROP PROCEDURE IF EXISTS rocket_factory.USP_User_Insert;
+
+/*
+=============================================
+Desc.   :   USP 사용자 정보 삽입
+Example :   CALL rocket_factory.USP_User_Insert('user01', 'user01@example.com', '$2b$12$examplehash_user01', 'user01');
+History :   2026-05-11   PJWOO 생성
+=============================================
+*/
+DELIMITER $$
+
+CREATE PROCEDURE rocket_factory.USP_User_Insert
+(
+    IN p_UserName VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    IN p_Email VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    IN p_PasswordHash VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    IN p_Nickname VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+)
+BEGIN
+
+    DECLARE v_EmailExists BIT DEFAULT 0;
+    DECLARE v_UserNameExists BIT DEFAULT 0;
+
+    -- 이메일 중복 확인
+    SET v_EmailExists = (
+        SELECT EXISTS(
+            SELECT 1
+            FROM rocket_factory.T_User
+            WHERE Email COLLATE utf8mb4_unicode_ci = p_Email COLLATE utf8mb4_unicode_ci
+        )
+    );
+
+    -- UserName 중복 확인
+    SET v_UserNameExists = (
+        SELECT EXISTS(
+            SELECT 1
+            FROM rocket_factory.T_User
+            WHERE UserName COLLATE utf8mb4_unicode_ci = p_UserName COLLATE utf8mb4_unicode_ci
+        )
+    );
+
+    IF v_EmailExists = 1 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = '이메일 중복';
+    END IF;
+
+    IF v_UserNameExists = 1 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'UserName 중복';
+    END IF;
+
+    INSERT INTO rocket_factory.T_User (
+        UserName,
+        Nickname,
+        Email,
+        PasswordHash
+    )
+    VALUES (
+        p_UserName,
+        p_Nickname,
+        p_Email,
+        p_PasswordHash
+    );
+
+END$$
+
+DELIMITER ;
